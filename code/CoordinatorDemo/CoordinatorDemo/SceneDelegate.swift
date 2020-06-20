57e2fecf72dfdcc7ba0b6baba1c8f5abe2e4aff9
Steve Kohls <stevekohls@gmail.com>
2020-06-20 15:57:57 -0400

    Manually instantiate the root view controller and hook it up
    - create storyboard in the SceneDelegate
    - don't automatically use the storyboard
    - instantiate the initial view controller programmatically
    - hook up the storyboard as the AViewController delegate
    - create a navigation controller programmatically
    - use it as the rootViewController

--- code/CoordinatorDemo/CoordinatorDemo/SceneDelegate.swift
+++ code/CoordinatorDemo/CoordinatorDemo/SceneDelegate.swift
@@ -12,12 +12,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    lazy var coordinator: MyCoordinator = MyCoordinator()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+        guard let aViewController = storyboard.instantiateViewController(identifier: "AViewController") as? AViewController else {
+            fatalError("Could not instantiate AViewController from storyboard")
+        }
+
+        aViewController.delegate = coordinator
+
+        let navigationController = UINavigationController(rootViewController: aViewController)
+
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
