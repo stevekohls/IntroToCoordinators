451f2531d89c16b77f9b233972ef1d394dc2bf74
Steve Kohls <stevekohls@gmail.com>
2020-06-20 15:55:24 -0400

    Add Coordinator
    Empty for now

---
+++ code/CoordinatorDemo/CoordinatorDemo/Coordinator.swift
@@ -0,0 +1,11 @@
+//
+//  Coordinator.swift
+//  CoordinatorDemo
+//
+//  Created by Steve Kohls on 5/5/20.
+//  Copyright © 2020 Steve Kohls. All rights reserved.
+//
+
+import UIKit
+
+class Coordinator {}
---
+++ code/CoordinatorDemo/CoordinatorDemo/MyCoordinator.swift
@@ -0,0 +1,11 @@
+//
+//  MyCoordinator.swift
+//  CoordinatorDemo
+//
+//  Created by Steve Kohls on 5/6/20.
+//  Copyright © 2020 Steve Kohls. All rights reserved.
+//
+
+import Foundation
+
+class MyCoordinator: Coordinator {}
