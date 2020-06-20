0bb65a313d41e13ad7025154735daf1e20e8f47a
Steve Kohls <stevekohls@gmail.com>
2020-06-20 15:57:20 -0400

    Add navigation and passing data to MyCoordinator

--- code/CoordinatorDemo/CoordinatorDemo/MyCoordinator.swift
+++ code/CoordinatorDemo/CoordinatorDemo/MyCoordinator.swift
@@ -6,6 +6,38 @@
 //  Copyright © 2020 Steve Kohls. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class MyCoordinator: Coordinator {}
+class MyCoordinator: Coordinator {
+    var name: String?
+    var flipped: Bool = false
+}
+
+extension MyCoordinator: AViewControllerDelegate {
+    func didSubmitName(_ name: String?, from fromViewController: UIViewController) {
+        self.name = name
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let bViewController = storyboard.instantiateViewController(identifier: "BViewController") as? BViewController else { return }
+
+        bViewController.delegate = self
+
+        let navigationController = fromViewController.navigationController
+        navigationController?.pushViewController(bViewController, animated: true)
+    }
+}
+
+extension MyCoordinator: BViewControllerDelegate {
+    func didSubmitSwitchValue(_ flipped: Bool, from fromViewController: UIViewController) {
+        self.flipped = flipped
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let cViewController = storyboard.instantiateViewController(identifier: "CViewController") as? CViewController else { return }
+
+        cViewController.name = name
+        cViewController.flipped = flipped
+
+        let navigationController = fromViewController.navigationController
+        navigationController?.pushViewController(cViewController, animated: true)
+    }
+}
