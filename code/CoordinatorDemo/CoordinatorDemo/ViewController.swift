7af6fb273b610b0c7ed3736b2931823bd1fed3d9
Steve Kohls <stevekohls@gmail.com>
2020-06-20 15:51:33 -0400

    Add initial demo project

---
+++ code/CoordinatorDemo/CoordinatorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorDemo
+//
+//  Created by Steve Kohls on 5/5/20.
+//  Copyright © 2020 Steve Kohls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
